ce96baa096b245b1b0ffbfe93747bee45ddad6cb
creeperusta <creeperusta606@gmail.com>
2025-07-20 14:25:10 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace webbrwoser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Adress = new System.Windows.Forms.Label();
+            this.Gotoadresse = new System.Windows.Forms.TextBox();
+            this.Goadress = new System.Windows.Forms.Button();
+            this.webControl1 = new EO.WinForm.WebControl();
+            this.webView1 = new EO.WebBrowser.WebView();
+            this.find = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Adress
+            // 
+            this.Adress.AutoSize = true;
+            this.Adress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Adress.Location = new System.Drawing.Point(18, 15);
+            this.Adress.Name = "Adress";
+            this.Adress.Size = new System.Drawing.Size(50, 16);
+            this.Adress.TabIndex = 1;
+            this.Adress.Text = "Adress";
+            // 
+            // Gotoadresse
+            // 
+            this.Gotoadresse.Location = new System.Drawing.Point(74, 14);
+            this.Gotoadresse.Name = "Gotoadresse";
+            this.Gotoadresse.Size = new System.Drawing.Size(335, 20);
+            this.Gotoadresse.TabIndex = 2;
+            // 
+            // Goadress
+            // 
+            this.Goadress.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Goadress.Location = new System.Drawing.Point(415, 13);
+            this.Goadress.Name = "Goadress";
+            this.Goadress.Size = new System.Drawing.Size(78, 21);
+            this.Goadress.TabIndex = 3;
+            this.Goadress.Text = "GO";
+            this.Goadress.UseVisualStyleBackColor = true;
+            this.Goadress.Click += new System.EventHandler(this.Goadress_Click);
+            // 
+            // webControl1
+            // 
+            this.webControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webControl1.BackColor = System.Drawing.Color.White;
+            this.webControl1.Location = new System.Drawing.Point(21, 63);
+            this.webControl1.Name = "webControl1";
+            this.webControl1.Size = new System.Drawing.Size(776, 375);
+            this.webControl1.TabIndex = 6;
+            this.webControl1.Text = "webControl1";
+            this.webControl1.WebView = this.webView1;
+            // 
+            // webView1
+            // 
+            this.webView1.DisableInputMsgRouting = false;
+            // 
+            // find
+            // 
+            this.find.Location = new System.Drawing.Point(499, 12);
+            this.find.Name = "find";
+            this.find.Size = new System.Drawing.Size(89, 26);
+            this.find.TabIndex = 7;
+            this.find.Text = "find";
+            this.find.UseVisualStyleBackColor = true;
+            this.find.Click += new System.EventHandler(this.find_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.find);
+            this.Controls.Add(this.webControl1);
+            this.Controls.Add(this.Goadress);
+            this.Controls.Add(this.Gotoadresse);
+            this.Controls.Add(this.Adress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Adress;
+        private System.Windows.Forms.TextBox Gotoadresse;
+        private System.Windows.Forms.Button Goadress;
+        private EO.WinForm.WebControl webControl1;
+        private EO.WebBrowser.WebView webView1;
+        private System.Windows.Forms.Button find;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace webbrwoser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Goadress_Click(object sender, EventArgs e)
+        {
+            webView1.Url = ("www." + Gotoadresse.Text + ".com");
+
+        }
+
+        private void find_Click(object sender, EventArgs e)
+        {
+            webView1.Url = ("https://www.google.com/search?q=" + Gotoadresse.Text);
+        }
+    }
+}
