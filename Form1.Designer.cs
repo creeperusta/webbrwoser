a222cac74d6de5ef9f14a9ac4839e79c2d37747d
creeperusta <creeperusta606@gmail.com>
2025-07-21 15:29:17 +0300

    lestgo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,13 +36,14 @@
             this.find = new System.Windows.Forms.Button();
             this.httpsadress = new System.Windows.Forms.TextBox();
             this.https = new System.Windows.Forms.Label();
+            this.closebutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Adress
             // 
             this.Adress.AutoSize = true;
             this.Adress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Adress.Location = new System.Drawing.Point(18, 15);
+            this.Adress.Location = new System.Drawing.Point(0, 26);
             this.Adress.Name = "Adress";
             this.Adress.Size = new System.Drawing.Size(50, 16);
             this.Adress.TabIndex = 1;
@@ -50,7 +51,7 @@
             // 
             // Gotoadresse
             // 
-            this.Gotoadresse.Location = new System.Drawing.Point(74, 14);
+            this.Gotoadresse.Location = new System.Drawing.Point(56, 26);
             this.Gotoadresse.Name = "Gotoadresse";
             this.Gotoadresse.Size = new System.Drawing.Size(335, 20);
             this.Gotoadresse.TabIndex = 2;
@@ -58,7 +59,7 @@
             // Goadress
             // 
             this.Goadress.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.Goadress.Location = new System.Drawing.Point(415, 13);
+            this.Goadress.Location = new System.Drawing.Point(397, 24);
             this.Goadress.Name = "Goadress";
             this.Goadress.Size = new System.Drawing.Size(78, 21);
             this.Goadress.TabIndex = 3;
@@ -72,9 +73,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.webControl1.BackColor = System.Drawing.Color.White;
-            this.webControl1.Location = new System.Drawing.Point(21, 63);
+            this.webControl1.Location = new System.Drawing.Point(3, 74);
             this.webControl1.Name = "webControl1";
-            this.webControl1.Size = new System.Drawing.Size(776, 375);
+            this.webControl1.Size = new System.Drawing.Size(820, 375);
             this.webControl1.TabIndex = 6;
             this.webControl1.Text = "webControl1";
             this.webControl1.WebView = this.webView;
@@ -85,9 +86,9 @@
             // 
             // find
             // 
-            this.find.Location = new System.Drawing.Point(499, 12);
+            this.find.Location = new System.Drawing.Point(481, 23);
             this.find.Name = "find";
-            this.find.Size = new System.Drawing.Size(89, 26);
+            this.find.Size = new System.Drawing.Size(89, 22);
             this.find.TabIndex = 7;
             this.find.Text = "find";
             this.find.UseVisualStyleBackColor = true;
@@ -95,7 +96,7 @@
             // 
             // httpsadress
             // 
-            this.httpsadress.Location = new System.Drawing.Point(74, 37);
+            this.httpsadress.Location = new System.Drawing.Point(56, 48);
             this.httpsadress.Name = "httpsadress";
             this.httpsadress.Size = new System.Drawing.Size(335, 20);
             this.httpsadress.TabIndex = 8;
@@ -104,18 +105,34 @@
             // 
             this.https.AutoSize = true;
             this.https.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.https.Location = new System.Drawing.Point(18, 41);
+            this.https.Location = new System.Drawing.Point(0, 52);
             this.https.Name = "https";
             this.https.Size = new System.Drawing.Size(38, 16);
             this.https.TabIndex = 9;
             this.https.Text = "https:";
+            // 
+            // closebutton
+            // 
+            this.closebutton.BackColor = System.Drawing.Color.Snow;
+            this.closebutton.DialogResult = System.Windows.Forms.DialogResult.Abort;
+            this.closebutton.ForeColor = System.Drawing.Color.Red;
+            this.closebutton.Location = new System.Drawing.Point(765, 2);
+            this.closebutton.Name = "closebutton";
+            this.closebutton.Size = new System.Drawing.Size(67, 22);
+            this.closebutton.TabIndex = 10;
+            this.closebutton.Text = "X";
+            this.closebutton.UseVisualStyleBackColor = false;
+            this.closebutton.Click += new System.EventHandler(this.closebutton_Click);
+            this.closebutton.KeyDown += new System.Windows.Forms.KeyEventHandler(this.closebutton_KeyDown);
+            this.closebutton.KeyUp += new System.Windows.Forms.KeyEventHandler(this.closebutton_KeyUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Coral;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(844, 450);
+            this.Controls.Add(this.closebutton);
             this.Controls.Add(this.https);
             this.Controls.Add(this.httpsadress);
             this.Controls.Add(this.find);
@@ -123,6 +140,7 @@
             this.Controls.Add(this.Goadress);
             this.Controls.Add(this.Gotoadresse);
             this.Controls.Add(this.Adress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -140,6 +158,7 @@
         private System.Windows.Forms.Button find;
         private System.Windows.Forms.TextBox httpsadress;
         private System.Windows.Forms.Label https;
+        private System.Windows.Forms.Button closebutton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -22,13 +22,32 @@ namespace webbrwoser
         private void Goadress_Click(object sender, EventArgs e)
         {
             webView.Url = ("www." + Gotoadresse.Text + ".com");
-            webView.Url = (httpsadress.Text);
+            webView.Url = (httpsadress.Text);   
         }
 
 
         private void find_Click(object sender, EventArgs e)
         {
             webView.Url = ("https://www.google.com/search?q=" + Gotoadresse.Text);
+        }
+
+
+
+        private void closebutton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void closebutton_KeyDown(object sender, KeyEventArgs e)
+        {
+            closebutton.BackColor = Color.Red;
+            closebutton.ForeColor = Color.White;
+        }
+
+        private void closebutton_KeyUp(object sender, KeyEventArgs e)
+        {
+            closebutton.BackColor = Color.White;
+            closebutton.ForeColor = Color.Red;
         }
     }
 }
